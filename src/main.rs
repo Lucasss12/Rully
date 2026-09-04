c88491003432a8eba70584a13655474e0f179e90
Lucasss12 <lucas.mene.pro@gmail.com>
2026-09-04 14:25:20 +0200

    feat: add Clap for cli commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+use clap::{Parser, ValueEnum};
+
+#[derive(Clone, Debug, ValueEnum)]
+#[derive(PartialEq)]
+enum Method {
+    #[value(name = "GET")]
+    Get,
+    #[value(name = "POST")]
+    Post,
+    #[value(name = "PUT")]
+    Put,
+    #[value(name = "PATCH")]
+    Patch,
+    #[value(name = "DELETE")]
+    Delete,
+}
+
+#[derive(Parser)]
+#[command(version, about)]
+struct Cli {
+    method: Method,
+    url: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    
+    println!("méthode : {:?}", cli.method);
+    println!("URL : {:?}", cli.url);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_arguments() {
+        let cli = Cli::parse_from(["rully", "GET", "https://example.com"]);
+        assert_eq!(cli.method, Method::Get);
+        assert_eq!(cli.url, "https://example.com");
+    }
+    
+    #[test]
+    fn missing_argument_method() {
+        let result = Cli::try_parse_from(["rully", "https://example.com"]);
+        assert!(result.is_err());
+    }
+    
+    #[test]
+    fn missing_argument_url() {
+        let result = Cli::try_parse_from(["rully", "GET"]);
+        assert!(result.is_err());
+    }
+    
+    #[test]
+    fn extra_arguments() {
+        let result = Cli::try_parse_from(["rully", "GET", "https://example.com", ""]);
+        assert!(result.is_err());
+    }
 }
